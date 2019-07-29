ea17d2b5ce6905e77a9445a062d4e643c7263789
jollygrin <jollygrin@protonmail.com>
2019-07-29 12:24:19 +0200

    pagination

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,21 @@ import fetch from "isomorphic-fetch";
 import Error from "next/error";
 import StoryList from "../components/StoryList";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     // define stories
     let stories;
 
+    // create page variable
+    let page;
+
     // catch errors
     try {
+      page = Number(query.page) || 1;
       const response = await fetch(
-        "https://node-hnapi.herokuapp.com/news?page=1"
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
       );
       stories = await response.json();
     } catch (err) {
@@ -19,11 +24,12 @@ class Index extends React.Component {
       stories = [];
     }
 
-    return { stories };
+    // defining this.props
+    return { page, stories };
   }
 
   render() {
-    const { stories } = this.props;
+    const { stories, page } = this.props;
 
     if (stories.length === 0) {
       return <Error statusCode={503} />;
@@ -35,6 +41,23 @@ class Index extends React.Component {
         description="A clone of Hacker News built on Next.js"
       >
         <StoryList stories={stories} />
+
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
       </Layout>
     );
   }
